83091b41d64ef9edaa3eed6738cad0d17aa27367
Li Zhuohua <lizhuohua1994@gmail.com>
2019-08-14 11:44:26 +0800

    update for new rustc version

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub mod kernel_result;
 pub mod printk;
 pub mod bindings;
 pub mod sync;
-// pub mod panic;
+pub mod panic;
 
 pub use self::kernel_module::KernelModule;
 pub use self::kernel_result::KernelResult;
